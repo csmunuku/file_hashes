a026f773f1b5f3b680779749a00a00bdb1413b18
Chandra Munukutla <csmunuku@gmail.com>
2018-03-29 17:53:16 -0700

    removed commented code

--- file_hashes.py
+++ file_hashes.py
@@ -26,20 +26,3 @@ def my_func(input_dir):
 if __name__ == "__main__":
     if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
         my_func(sys.argv[1])
-#    if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
-#        matches = {}
-#        print(os.listdir(sys.argv[1]))
-#        for file in os.listdir(sys.argv[1]):
-#            if os.path.isfile(file):
-#                with open(file, 'rb') as f:
-#                    file_sha256 = str(hashlib.sha256(f.read()).hexdigest())
-#                    if file_sha256 in matches:
-#                        if matches.get(str(file_sha256)) is not None:
-#                            my_list = list(matches.get(str(file_sha256)))
-#                            my_list.append(str(file))
-#                            matches[file_sha256] = my_list
-#                    else:
-#                        new_list = list()
-#                        new_list.append(str(file))
-#                        matches.setdefault(file_sha256, new_list)
-#        pprint.pprint(matches)
